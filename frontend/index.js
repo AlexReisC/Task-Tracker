3696aa9809ef2b8677c2708c4cbcf47876044177
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-17 22:46:19 -0300

    feat: add archives off the frontend

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+
+function addTask() {
+    const input = document.getElementById("task-input").textContent;
+    const ul = document.getElementById("task-list");
+    document.createElement("li");
+    ul.appendChild()
+}
