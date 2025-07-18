90e2f286179f9ff2f6aef0930ac954385ede040c
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-18 10:51:16 -0300

    feat: implement create task functionallity, only in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,46 @@
+const form = document.getElementById("new-task-form");
 
-function addTask() {
-    const input = document.getElementById("task-input").textContent;
-    const ul = document.getElementById("task-list");
-    document.createElement("li");
-    ul.appendChild()
-}
+form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    document.getElementById('task-title').value
+
+    const titleInput = document.getElementById("task-title");
+    const descriptionInput = document.getElementById("task-description");
+    
+    if (titleInput.value.trim() === "") {
+        alert("Titulo vazio!");
+        return;
+    }
+
+    const task = document.createElement("div");
+    task.setAttribute("class", "task");
+    
+    const spanTitle = document.createElement("span");
+    spanTitle.setAttribute("class", "task-title");
+    spanTitle.textContent = titleInput.value;
+    spanTitle.title = descriptionInput.value;
+    task.appendChild(spanTitle);
+
+    /* const description = document.createElement("p");
+    description.setAttribute("class", "task-description");
+    description.textContent = descriptionInput.value;
+    task.appendChild(description); */
+    
+    const completeBtn = document.createElement("button");
+    completeBtn.setAttribute("class", "btn-complete");
+    completeBtn.textContent = "Concluir";
+    task.appendChild(completeBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "btn-delete");
+    deleteBtn.textContent = "Deletar";
+    task.appendChild(deleteBtn);
+
+    const container = document.getElementById("tasks-container");
+    container.appendChild(task);
+
+    titleInput.value = "";
+    descriptionInput.value = "";
+});
+
+// mudar classe task para task done se a tarefa for concluida
