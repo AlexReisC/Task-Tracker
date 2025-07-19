3b4a389db6644bcd2014ad09d6f65da2ebb1bb72
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-19 01:10:48 -0300

    feat: refactor task creation logic into createTask function

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,6 @@ const tasksContainer = document.querySelector("#tasks-container");
 
 form.addEventListener("submit", (ev) => {
     ev.preventDefault();
-    document.getElementById('task-title').value
 
     const titleInput = document.getElementById("task-title");
     const descriptionInput = document.getElementById("task-description");
@@ -13,35 +12,7 @@ form.addEventListener("submit", (ev) => {
         return;
     }
 
-    const task = document.createElement("div");
-    task.setAttribute("class", "task");
-    
-    const spanTitle = document.createElement("span");
-    spanTitle.setAttribute("class", "task-title");
-    spanTitle.textContent = titleInput.value;
-    spanTitle.title = descriptionInput.value;
-    task.appendChild(spanTitle);
-
-    /* const description = document.createElement("p");
-    description.setAttribute("class", "task-description");
-    description.textContent = descriptionInput.value;
-    task.appendChild(description); */
-    
-    const completeBtn = document.createElement("button");
-    completeBtn.setAttribute("class", "btn-complete");
-    completeBtn.textContent = "Concluir";
-    task.appendChild(completeBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "btn-delete");
-    deleteBtn.textContent = "Deletar";
-    task.appendChild(deleteBtn);
-
-    const container = document.getElementById("tasks-container");
-    container.appendChild(task);
-
-    titleInput.value = "";
-    descriptionInput.value = "";
+    createTask(titleInput, descriptionInput);
 });
 
 tasksContainer.addEventListener("click", (ev) => {
@@ -85,3 +56,37 @@ tasksContainer.addEventListener("click", (ev) => {
         }
     }
 });
+
+function createTask(title, description) {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    
+    const spanTitle = document.createElement("span");
+    spanTitle.classList.add("task-title");
+    spanTitle.textContent = titleInput.value;
+    spanTitle.title = descriptionInput.value;
+    task.appendChild(spanTitle);
+    
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("btn-complete");
+    completeBtn.textContent = "Concluir";
+    task.appendChild(completeBtn);
+    
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("btn-edit");
+    editBtn.textContent = "Editar";
+    task.appendChild(editBtn);
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn-delete");
+    deleteBtn.textContent = "Deletar";
+    task.appendChild(deleteBtn);
+
+    const container = document.getElementById("tasks-container");
+    container.appendChild(task);
+
+    titleInput.value = "";
+    descriptionInput.value = "";
+
+    return task;
+}
