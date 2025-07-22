ae5624c2f05a6e61348cb645cee54d603368ad98
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-22 18:17:43 -0300

    chore: add @fastify/cors dependency and configure CORS in server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import app from './app.js';
+import cors from '@fastify/cors'
 
 const start = async () => {
-  try {
-    await app.listen({ port: 3000 });
-    console.log('Servidor Fastify rodando em http://localhost:3000');
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+	await app.register(cors, {
+		origin: '*',
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+	});
+
+	try {
+		await app.listen({ port: 3000 });
+		console.log('Servidor Fastify rodando em http://localhost:3000');
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 };
 
 start();
